fa5a27567d635597d2fd1b5d821463ef01bb5561
rChinnnn <B10121035@yuntech.edu.tw>
2023-03-15 18:21:46 +0800

    try WebRTC in electron

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,41 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process unless
-// nodeIntegration is set to true in webPreferences.
-// Use preload.js to selectively enable features
-// needed in the renderer process.
+import { desktopCapturer } from 'electron';
+
+// 獲取本地視頻流
+async function getLocalStream() {
+  const sources = await desktopCapturer.getSources({ types: ['window', 'screen'] });
+
+  return navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      // ts 報錯 `類型 'MediaTrackConstraints' 中沒有 'mandatory'`
+      // mandatory: {
+      //   chromeMediaSource: 'desktop',
+      //   chromeMediaSourceId: sources[0].id
+      // },
+      width: { ideal: 1280 },
+      height: { ideal: 720 }
+    }
+  });
+}
+
+// 顯示本地視頻流
+async function showLocalStream() {
+  const localStream = await getLocalStream();
+  const localVideo = document.getElementById('localVideo') as HTMLVideoElement;
+  localVideo.srcObject = localStream;
+}
+
+// 開始 WebRTC 連接
+async function startWebRTC() {
+  const pc = new RTCPeerConnection();
+  const localStream = await getLocalStream();
+  const remoteVideo = document.getElementById('remoteVideo') as HTMLVideoElement;
+  localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
+  pc.ontrack = event => remoteVideo.srcObject = event.streams[0];
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  // 將 offer 通過信令服務傳遞給對方
+}
+
+showLocalStream();
+startWebRTC();
