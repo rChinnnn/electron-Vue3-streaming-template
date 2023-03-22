81b7adda481e4d134265501166a2058817eff510
rChinnnn <b10121035@yuntech.edu.tw>
2023-03-22 14:11:27 +0800

    ready for Vue3 streaming ENV.

---
+++ src/electron/main/main.ts
@@ -0,0 +1,69 @@
+import { join } from 'path';
+import {
+    app,
+    BrowserWindow
+} from 'electron';
+import NodeMediaServer from 'node-media-server';
+
+const isDev = process.env.npm_lifecycle_event === "app:dev" ? true : false;
+
+function createWindow() {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: join(__dirname, '../preload/preload.js'),
+        },
+    });
+
+    // and load the index.html of the app.
+    if (isDev) {
+        mainWindow.loadURL('http://localhost:3000');// Open the DevTools.
+        mainWindow.webContents.openDevTools();
+    } else {
+        mainWindow.loadFile(join(__dirname, '../../index.html'));
+    }
+    // mainWindow.loadURL( //this doesn't work on macOS in build and preview mode
+    //     isDev ?
+    //     'http://localhost:3000' :
+    //     join(__dirname, '../../index.html')
+    // );
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    createWindow()
+    const nms = new NodeMediaServer({
+        rtmp: {
+            port: 1935,
+            chunk_size: 60000,
+            gop_cache: true,
+            ping: 60,
+            ping_timeout: 30
+        },
+        http: {
+            mediaroot: '*',
+            port: 8000,
+            allow_origin: '*'
+        }
+    })
+    nms.run()
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
